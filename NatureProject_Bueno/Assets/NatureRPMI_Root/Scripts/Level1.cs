008a21c84554265bf1261b19ae980752065c05b7
uluna99 <uluna099@gmail.com>
2025-02-04 23:21:19 +0100

    cambios varios

--- NatureProject_Bueno/Assets/NatureRPMI_Root/Scripts/Level1.cs
+++ NatureProject_Bueno/Assets/NatureRPMI_Root/Scripts/Level1.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;  // Para cambiar de escena
 
+<<<<<<< Updated upstream
 public class FinalizarNivel1 : MonoBehaviour
 {
 
@@ -14,6 +15,21 @@ public class FinalizarNivel1 : MonoBehaviour
 
 
             SceneManager.LoadScene("Level2");
+=======
+public class FinalizarNivel : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider other)
+    {
+      
+        if (other.CompareTag("Player"))
+        {
+            
+            Debug.Log("Level Finished!");
+
+            
+            SceneManager.LoadScene("FinalBoss");
+>>>>>>> Stashed changes
         }
     }
 }
